0fd9a6ac3fd09057c5049c80f1668aacf59eb8af
gdm <gdm.mbp@gdmMBP.local>
2018-11-08 19:12:04 -0500

    add segues

---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by gdm on 11/8/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+    
+}
